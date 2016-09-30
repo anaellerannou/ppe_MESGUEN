7e652e721bfc941eb885dbf301a848f27d024769
anaellerannou <rannouanaelle@gmail.com>
2016-09-30 11:24:16 +0200

    Add files via upload

---
+++ 1.5.1mesguen/AC1/Modifier_les_tournees.php
@@ -0,0 +1,224 @@
+<?php 
+// On démarre la session AVANT d'écrire du code HTML
+session_start();
+?>
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<?php
+			include '../head.php';
+		?>
+		<link rel="stylesheet" href="AC12.css" />
+		<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet"/>		
+	</head>
+	
+	<?php
+	
+		
+	
+		include '../connectAD.php';
+		if (empty($_GET['id'])) {
+			$TRNNUM=$_SESSION['TRNNUM'];
+			$_SESSION['TRNNUM']=$TRNNUM;
+		} else {
+			$TRNNUM=$_GET['id'];
+			$_SESSION['TRNNUM']=$TRNNUM;
+		}
+		
+		$sql="SELECT TRNNUM,VEHIMMAT,CHFID,TRNDTE FROM tournee where TRNNUM=$TRNNUM;";
+		$results = tableSQL($sql);
+		
+		
+			foreach ($results as $ligne) {
+				//on extrait chaque valeur de la ligne courante
+				$TRNNUM = $ligne[0];
+				$VEHIMMAT = $ligne[1];
+				$CHFID = $ligne[2];
+				$TRNDTE = $ligne[3];
+				
+			}
+			$VEHIMMAT_SAVE=$VEHIMMAT;
+			$CHFID_SAVE=$CHFID;
+			
+		$sql="SELECT CHFID,CHFNOM FROM chauffeur where CHFID=$CHFID;";
+		$results = tableSQL($sql);
+			
+		
+			
+			foreach ($results as $ligne) {
+				//on extrait chaque valeur de la ligne courante
+				$CHFID = $ligne[0];
+				$CHFNOM = $ligne[1];
+			
+			}
+
+			
+			
+			
+			?>
+	
+	<body>
+	
+	
+	
+	
+		<h1>Application Chauffeur</h1>
+		<br/>
+	
+	<div class="container">	
+	
+		<div class="row">
+		
+		
+			<div class="col-lg-6">
+		
+				<form class="form-horizontal" id='formulairetest' action ='UpdateInfo.php' method='get'>	
+			
+					<div class="row">
+	      				<div class="col-lg-4"><label for="TRNDTE">Date</label></div>
+			 			<div class="col-lg-8"><input type="text" name="TRNDTE" id="TRNDTE" class="calendrier" value="<?php echo $TRNDTE; ?>" size="10" maxlength="10"/></div>
+						<br/><br/>
+					</div>
+					
+					<div class="row">
+	      				<div class="col-lg-4"><label for="CHFID">Chauffeur</label></div>
+						<div class="col-lg-8">
+			<?php	
+			$sql = "SELECT * FROM chauffeur ORDER BY CHFNOM;"; 
+			$result = executeSQL($sql);
+			$results = tableSQL($sql);
+			$cpt=compteSQL($sql);
+										
+			if ($cpt>0) {
+				echo "<select size=\"1\" name=\"CHFID\" id=\"CHFID\">";	
+				foreach ($results as $ligne) {
+				//on extrait chaque valeur de la ligne courante
+					$CHFID = $ligne[0];
+					$CHFNOM = $ligne[1];
+					if ($CHFID==$CHFID_SAVE) {
+						echo "<option value=$CHFID selected=\"selected\">$CHFNOM</option>";
+					} else {
+						echo "<option value=$CHFID>$CHFNOM</option>";
+					}
+				}
+			} else {
+				echo "<select size=\"1\" name=\"CHFID\" id=\"CHFID\" disabled=\"disabled\" >";	
+				echo "<option>Aucune information...</option>";
+			}
+										
+			echo "</select>";			   
+    									
+    		if (isset($_GET['message']))
+    			echo $_GET['message'];
+    		else
+    			echo "&nbsp;";
+    		?><br/><br/>
+    		
+    		</div></div>
+    		
+    		<div class="row">
+	      		<div class="col-lg-4"><label for="VEHIMMAT">V&eacute;hicule</label></div>
+			
+				<div class="col-lg-8">
+			<?php	
+			$sql = "SELECT * FROM vehicule"; 
+			$result = executeSQL($sql);
+			$results = tableSQL($sql);
+			$cpt=compteSQL($sql);
+				
+			if ($cpt>0) {
+				echo "<select size=\"1\" name=\"VEHIMMAT\" id=\"VEHIMMAT\">";	
+				foreach ($results as $ligne) {
+				//on extrait chaque valeur de la ligne courante
+					$VEHIMMAT = $ligne[0];
+					if ($VEHIMMAT==$VEHIMMAT_SAVE) {
+						echo "<option value=$VEHIMMAT selected=\"selected\">$VEHIMMAT</option>";
+					} else {
+						echo "<option value=$VEHIMMAT>$VEHIMMAT</option>";
+					}
+				}						
+			} else {
+				echo "<select size=\"1\" name=\"VEHIMMAT\" id=\"VEHIMMAT\" disabled=\"disabled\" >";	
+				echo "<option>Aucune information...</option>";
+			}
+			
+			echo "</select>";			   
+            
+    		if (isset($_GET['message']))
+    			echo $_GET['message'];
+    		else
+    			echo "&nbsp;";
+    		?></div>
+    		<br/><br/>
+    		</div>
+    		
+    		<div class="row">
+	      		<div class="col-lg-4"><label for="TRNARCHAUFFEUR">Pris en charge le </label></div>
+				<div class="col-lg-8"><input type="text" name="TRNARCHAUFFEUR" id="TRNARCHAUFFEUR" class="calendrier" size="10" maxlength="10"/></div>
+			<br/><br/>
+			</div>
+			
+			<div class="row">
+	      		<div class="col-lg-4"><label for="Heures">à</label></div>
+			 	<div class="col-lg-8"><input type="text" name="Heures" id="Heures" size="5" maxlength="5" value="00H00"/></div>
+			<br/><br/>
+			</div>
+			
+			
+			<div class="row">
+	      		<div class="col-lg-4"><label for="TRNCOMMENTAIRE">Commentaire</label></div>
+				<div class="col-lg-8"><textarea id="TRNCOMMENTAIRE" name="TRNCOMMENTAIRE" rows="6" cols="25"></textarea></div>
+			<br/><br/>
+			</div>
+			
+			<div class="row">
+			<div class="col-lg-offset-4 col-lg-8">
+			<p>
+				<br/><br/>
+				<input class='btn btn-success' id='tournee_valid' name='tournee_valid' type='submit' value='Valider'></input>
+			</p>
+			</div>
+			</div>
+		</form>
+		
+		<div class="row">
+		<div class="col-lg-offset-4 col-lg-8">
+		<form id='tournee_annul' action ='tableau_de_donnees.php' method='get'>	
+			<p>					
+				<br/><input class='btn btn-warning' id='tournee_annul' name='tournee_annul' type='submit' value='Annuler'></input>
+			</p>
+		</form>
+		
+		</div>
+		</div>
+		</div>
+		
+		
+		<div class="etapes">
+			
+		<strong>Etapes</strong><br/><br/>
+		<?php	
+		include 'ListeEtape.php';				
+		?>
+								
+		<?php
+		if (isset($_GET['message2']))
+			echo $_GET['message2'];
+		else
+			echo "&nbsp;";
+		?>
+		<p><a href=noname></a></p>
+		<?php
+		echo "
+			<a><strong>
+				<input class='form-horizontal btn btn-default' id='etape_ajout' type='button' name='etape_ajout' value='Ajouter...'  onclick='location.href=\"organiser_une_etape.php?id=$TRNNUM\"'/>
+			</strong></a>";
+		?>
+		</div>
+		
+		</div>
+		</div>
+		
+	</body>
+</html>
---
+++ 1.5.1mesguen/AC1/modifier_une_etape.php
@@ -0,0 +1,192 @@
+<?php 
+// On démarre la session AVANT d'écrire du code HTML
+session_start();
+?>
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="ISO-8859-1">
+			<title>organiser une &eacute;tape</title>
+			<link href="organiser_une_etape.css" rel="stylesheet">
+			<?php include 'head.php' ?>
+			<link href="calendrier.css" rel="stylesheet">
+			
+			
+		</head>
+
+	<body>
+				<p>Organiser les tourn&eacute;es - Tourn&eacute;e 
+				<?php include 'connectAD.php';
+				//tournee arbitraire pour les tests
+				$TRNNUM=$_SESSION['TRNNUM'];
+				$_SESSION['TRNNUM']=$TRNNUM;
+				$sql = 'SELECT `TRNNUM` FROM `tournee` WHERE `TRNNUM` ='.$TRNNUM.'; ';
+				$result = champSQL($sql)  ;
+				echo $result ;
+				
+				
+				//$x=1;
+				
+				//$etpid=$result.$x;
+				//$sql4 = "INSERT INTO `etape` (`TRNNUM`, `ETPID`) VALUES (`".$result."`, `".$etpid."`) "
+				
+				 ?> Etape <?php
+				 $ETPID=$_GET['id'];
+				 $sql2 = 'SELECT `ETPID` FROM `etape` WHERE `ETPID`='.$ETPID.';';
+				 $result2 = champSQL ($sql2);
+				 echo $result2 ; ?></p>
+	
+		<br/><br/>
+		<form action ="modifier_etape.php" method="get" id="Formulaire">
+			<fieldset>
+		<div class='lieu'>
+			<label for='lieu'>Lieu </label>
+			
+				<?php 
+				$sql="SELECT ETPID,LIEUID,ETPHREDEBUT,ETPHREFIN FROM etape where TRNNUM=$TRNNUM AND ETPID=$ETPID;";
+				$results = tableSQL($sql);
+				
+				echo "<table id=\"affichetableau\">";
+				echo "<tbody>";
+				
+				foreach ($results as $ligne) {
+					//on extrait chaque valeur de la ligne courante
+					$ETPID = $ligne[0];
+					$LIEUID = $ligne[1];
+					$ETPHREDEBUT = $ligne[2];
+					$ETPHREFIN = $ligne[3];
+				
+				}
+				$LIEUID_SAVE=$LIEUID;
+				$_SESSION['ETPID']=$ETPID;
+				$ETPHREDEBUT=substr($ETPHREDEBUT,11,19);
+				$ETPHREFIN=substr($ETPHREFIN,11,19);
+				?>			
+					
+			
+				<select name='lieu' size=1 >
+				
+				<?php 
+				
+				$sql3 = 'SELECT `LIEUID` , `LIEUNOM` FROM `lieu` ';
+				$cpt = compteSQL($sql3);
+				$results = tableSQL($sql3);
+				
+				if ($cpt>0) {
+					foreach ($results as $ligne) {
+						//on extrait chaque valeur de la ligne courante, 
+						//lieu doit correspondre a chaque id /!\
+						
+						$lieu = $ligne[0];
+						$info = $ligne[1];
+						//on affiche la ligne courante dans le select
+						if ($lieu==$LIEUID_SAVE) {
+							echo "<option value=$lieu selected=\"selected\">$info</option>";
+						} else {
+							echo "<option value=$lieu>$info</option>";
+						}
+					}
+					} else {
+					echo '<option>Aucune information...</option>';
+					}
+					?>	
+					
+				</select>
+				
+				
+				<?php
+				if (isset($_GET['message']))
+					echo $_GET['message'];
+				else
+					echo "&nbsp;";							
+				?>
+		</div>
+		
+		<br/>
+		<br/>
+			<div id="date_prise_en_charge">
+			<?php
+				echo "
+				<label for='date_prise_en_charge'>Pris en charge le</label>
+				<input id='date_prise_en_charge' name='date_prise_en_charge' type='text' value='' class='calendrier' size='20'></input>
+				<br/><br/>
+				<label for='Heure'>à</label>
+				<input type='text' name='Heure' id='Heure' size='10' maxlength='5' value='00H00'/>"
+				?>
+        	<?php
+    				if (isset($_GET['message']))
+    					echo $_GET['message'];
+    				else
+    					echo "&nbsp;";
+    		?>
+			
+			</div>
+
+			<br/>
+
+			<div class="date_heure_rdv_tot">
+				<?php
+					echo "
+					<label for='heure_rdv_tot'>Rendez-vous entre</label>
+					<input id='heure_rdv_tot' name='heure_rdv_tot' type='text' size='20' maxlength='8' value='$ETPHREDEBUT'  />"
+				?>																			
+			
+				
+        	<?php
+    				if (isset($_GET['message']))
+    					echo $_GET['message'];
+    				else
+    					echo "&nbsp;";
+    		?>
+			
+			</div>
+
+			<br/>
+
+			<div class="date_heure_rdv_tard">
+			<?php
+				echo "
+				<label for='heure_rdv_tard'>et </label>
+				<input id='heure_rdv_tard' name='heure_rdv_tard' type='text' size='20' maxlength='8' value='$ETPHREFIN'   /> "
+			?>
+			
+        	<?php
+    				if (isset($_GET['message']))
+    					echo $_GET['message'];
+    				else
+    					echo "&nbsp;";   
+    		?>
+			
+			</div>
+
+			<br/>
+
+			
+	
+			<div class="commentaire">
+			<?php			
+				echo "
+				<label for='commentaire'>Commentaire</label>
+				<textarea id='commentaire' name='commentaire' type='text' value=''></textarea> ";
+			?>	
+				
+        	<?php
+    				if (isset($_GET['message']))
+    					echo $_GET['message'];
+    				else
+    					echo "&nbsp;";
+    		?>
+				
+			</div>
+
+			<br/><br/><br/><br/><br/>
+
+	
+			<input id="valider" name="valider" type="image" value="Valider" title="valider" src="images/valider.png"></input>
+			</fieldset>
+		</form>
+		<form action ="Modifier_les_tournees.php" method="get" id="Formulaire">
+				<input id="annuler" name="annuler" type="image" value="Annuler" title="annuler" src="images/deleteV2.png"></input>
+		</form>
+	</body>
+</html>
